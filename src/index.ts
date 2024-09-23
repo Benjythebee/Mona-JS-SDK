9f870ec79d6732640f1dcfffe59300e2176caad5
Benjythebee <Benjy.larcher@aol.com>
2024-09-23 13:30:41 +1200

    try add a default

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { MonaAPI } from './mona';
+
 export type {MonaAPIProps} from './mona';
-export {MonaAPI} from './mona';
+export {MonaAPI}
 export * from './types'
 export {Observable,Observer} from './libs/observable'
+export default MonaAPI
